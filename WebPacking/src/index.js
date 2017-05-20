ba9071e7ffbb3b08ec814bc567a6cd49e0a235a8
DevOps\Developer <bobbanovski@gmail.com>
2017-05-20 16:08:50 +1000

    import/export with commonJS

---
+++ WebPacking/src/index.js
@@ -0,0 +1,20 @@
+//links other js modules
+
+// =====module systems ===========
+/*
+Common JS - implemented by NodeJS
+require
+module.exports
+
+AMD - Asynchronous Module Definition
+require
+define
+
+ES2015
+import
+export
+*/
+
+//commonJS import
+const summation = require('./summation'); //relative path reference
+
---
+++ WebPacking/src/sum.js
@@ -0,0 +1,10 @@
+//mathematical operations
+
+//es2015
+const summation = (a, b) => a + b;
+
+//commonJS export
+module.exports = summation;
+
+const total = summation(10, 5)
+console.log(total);
